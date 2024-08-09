2d2376269aa1e432aabc023f628f02cb4f896fe1
EnergyPatrikHU <energypatrikhu@proton.me>
2024-08-09 03:40:17 +0200

    Update build configuration

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,57 +1,65 @@
 import { build } from 'esbuild';
-import { existsSync, rmSync, readFileSync, writeFileSync } from 'node:fs';
-import { EOL } from 'node:os';
+import { existsSync, rmSync, readFileSync, writeFileSync, readdirSync } from 'fs';
+import { EOL } from 'os';
+import { join } from 'path';
 
 const __sourceFile = './src/index.ts';
-const __destinationFile = './build/index.js';
+const __destinationDir = './build';
 const prefix = '#! /usr/bin/env node';
 
-console.log(`Removing '${__destinationFile}'...`);
+console.log(`Removing everything from '${__destinationDir}' dir...`);
 try {
-	if (existsSync(__destinationFile)) {
-		rmSync(__destinationFile, { force: true, recursive: true });
-	}
-	console.log(`Removed '${__destinationFile}'`);
+  if (existsSync(__destinationDir)) {
+    const files = readdirSync(__destinationDir).map((file) => join(__destinationDir, file));
+    for (const file of files) {
+      rmSync(file, { force: true, recursive: true });
+    }
+  }
+  console.log(`Removed everything from '${__destinationDir}' dir`);
 } catch (error) {
-	console.error(`Failed to remove '${__destinationFile}'`);
-	throw error;
+  console.error(`Failed to remove everything from '${__destinationDir}' dir`);
+  throw error;
 }
 
-console.log(`\nMinifying '${__sourceFile}' into '${__destinationFile}' file...`);
+console.log(`\nMinifying '${__sourceFile}' into '${__destinationDir}' dir...`);
 try {
-	const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
-	await build({
-		entryPoints: [__sourceFile],
-		platform: 'node',
-		bundle: true,
-		outfile: __destinationFile,
-		logLevel: 'warning',
-		treeShaking: true,
-		minify: true,
-		format: 'cjs',
-		external: (() => {
-			const dependencies = new Set();
-			for (const key in packageJson) {
-				if (key.toLowerCase().endsWith('dependencies')) {
-					for (const dep in packageJson[key]) {
-						dependencies.add(dep);
-					}
-				}
-			}
-			return [...dependencies.values()];
-		})(),
-	});
-	console.log(`Minified '${__sourceFile}' into '${__destinationFile}' file`);
+  const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
+  await build({
+    entryPoints: [__sourceFile],
+    platform: 'node',
+    bundle: true,
+    outdir: __destinationDir,
+    logLevel: 'warning',
+    treeShaking: true,
+    minify: true,
+    format: 'cjs',
+    external: (() => {
+      const dependencies = new Set();
+      for (const key in packageJson) {
+        if (key.toLowerCase().endsWith('dependencies')) {
+          for (const dep in packageJson[key]) {
+            dependencies.add(dep);
+          }
+        }
+      }
+      return [...dependencies.values()];
+    })(),
+  });
+  console.log(`Minified '${__sourceFile}' into '${__destinationDir}' dir`);
 } catch (error) {
-	console.error(`Failed to minify '${__sourceFile}' into '${__destinationFile}' file`);
-	throw error;
+  console.error(`Failed to minify '${__sourceFile}' into '${__destinationDir}' dir`);
+  throw error;
 }
 
 console.log('\nAdding shebang...');
 try {
-	writeFileSync(__destinationFile, prefix + EOL + readFileSync(__destinationFile, 'utf-8'), 'utf-8');
-	console.log('Added shebang');
+  writeFileSync(
+    join(__destinationDir, 'index.js'),
+    prefix + EOL + readFileSync(join(__destinationDir, 'index.js'), 'utf-8'),
+    'utf-8',
+  );
+  console.log('Added shebang');
 } catch (error) {
-	console.error('Failed to add shebang');
-	throw error;
+  console.error('Failed to add shebang');
+  throw error;
 }
